d2245743f1d0ee04cca256c677ff2061160f87aa
jarrah wu <jarrah@JarrahWu.(none)>
2015-06-02 18:30:59 +0800

    add http  call back status

--- app/src/main/java/com/stkj/xtools/HttpCallBack.java
+++ app/src/main/java/com/stkj/xtools/HttpCallBack.java
@@ -1,7 +1,6 @@
 package com.stkj.xtools;
 
-import android.widget.Toast;
-
+import com.android.volley.NetworkResponse;
 import com.android.volley.Response.ErrorListener;
 import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
@@ -19,8 +18,50 @@ public class HttpCallBack implements Listener<JSONObject>, ErrorListener {
 
 	@Override
 	public void onErrorResponse(VolleyError error) {
-        Log.from(this, "net work error : " + error);
-        Toast.makeText(XTool.getInstance().getContext(), R.string.network_err, Toast.LENGTH_SHORT).show();
+		try {
+			NetworkResponse response = error.networkResponse;
+			Log.from(this, String.format("net work error %s >> %d", error.toString(), response.statusCode));
+			switch (error.networkResponse.statusCode) {
+				case 403:
+					onAuthFailure(response);
+					break;
+				case 400:
+					onMethodNotAllow(response);
+					break;
+				case 404:
+					onUrlNotFound(response);
+					break;
+				case 500:
+					onServerError(response);
+					break;
+				default:
+					onDefaultError(response);
+					break;
+			}
+		}catch (Exception e) {
+			Log.from(this, "uncaught exception " + e + "|" + error);
+			Util.toast(R.string.network_err);
+		}
+	}
+
+	public void onAuthFailure(NetworkResponse response) {
+		Util.toast(R.string.auth_failure);
+	}
+
+	public void onMethodNotAllow(NetworkResponse response) {
+		Util.toast(R.string.network_err);
+	}
+
+	public void onUrlNotFound(NetworkResponse response) {
+		Util.toast(R.string.network_err);
+	}
+
+	public void onServerError(NetworkResponse response) {
+		Util.toast(R.string.server_error);
+	}
+
+	public void onDefaultError(NetworkResponse response) {
+		Util.toast(R.string.network_err);
 	}
 
 }
